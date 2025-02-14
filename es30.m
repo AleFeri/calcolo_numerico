75a67330e18ff66dba967350b062fcb3b35fd60b
AleFeri <50620354+AleFeri@users.noreply.github.com>
2025-02-14 16:50:35 +0100

    completato es 30

---
+++ es30.m
@@ -0,0 +1,13 @@
+% Valore esatto
+I_esatto = (exp(3) - 1) / 3;
+
+% Parametri richiesti
+kVals = [1, 2, 3, 6];
+n = 12;
+
+fprintf('   k    Appross        Errore (If - esatto)   Err stima\n');
+for k = kVals
+    [If, errStima] = composita(@(x) exp(3*x), 0, 1, k, n);
+    errVero = abs(If - I_esatto);
+    fprintf(' %3d  %12.6f  %18.3e  %12.3e\n', k, If, errVero, errStima);
+end
