4e242c627b5461f35e116e0fea3b8a37dff19a81
AleFeri <50620354+AleFeri@users.noreply.github.com>
2025-02-15 19:05:28 +0100

    corretto es 7

--- es07.m
+++ es07.m
@@ -1,10 +1,8 @@
 clearvars; close all; clc
 
-% Definizione della funzione e della sua derivata
 f  = @(x) exp(x) - cos(x) + sin(x) - x.*(x+2);
 df = @(x) exp(x) + sin(x) + cos(x) - (2*x + 2);  % derivata di f(x)
 
-% Parametri:
 tol_list = [1e-3, 1e-6, 1e-9, 1e-12];
 max_iter = 100;
 
@@ -17,12 +15,10 @@ x1_sec = 0.9;   % Secanti: secondo punto
 x0_newt_mod = 1;% Newton modificato: approssimazione iniziale
 m_newt_mod = 5; % Newton modificato: molteplicità della radice
 
-% Preallocazione dei risultati in una cell array.
-% Ogni riga conterrà: {Metodo, Toll, Radice, Iterazioni, Valutazioni Funzionali}
 results = {};
 
 for tol = tol_list
-    % Metodo della Bisezione (funzione es04.m)
+    % Metodo della Bisezione
     try
         [root_bis, iter_bis, n_eval_bis] = bisezione(f, a0, b0, tol);
         results = [results; {'Bisezione', tol, root_bis, iter_bis, n_eval_bis}];
@@ -31,7 +27,7 @@ for tol = tol_list
         results = [results; {'Bisezione', tol, "Non Converge", "-", "-"}];
     end
 
-    % Metodo di Newton (funzione es05_newton.m)
+    % Metodo di Newton
     try
         [root_newton, iter_newton, n_eval_newton] = newton(f, df, x0_newton, tol, max_iter);
         results = [results; {'Newton', tol, root_newton, iter_newton, n_eval_newton}];
@@ -40,7 +36,7 @@ for tol = tol_list
         results = [results; {'Newton', tol, "Non Converge", "-", "-"}];
     end
 
-    % Metodo delle Secanti (funzione es05_secanti.m)
+    % Metodo delle Secanti
     try
         [root_sec, iter_sec, n_eval_sec] = secanti(f, x0_sec, x1_sec, tol, max_iter);
         results = [results; {'Secanti', tol, root_sec, iter_sec, n_eval_sec}];
@@ -49,7 +45,7 @@ for tol = tol_list
         results = [results; {'Secanti', tol, "Non Converge", "-", "-"}];
     end
 
-    % Metodo di Newton Modificato (funzione es06_newton_modificato.m)
+    % Metodo di Newton Modificato
     try
         [root_newt_mod, iter_newt_mod, n_eval_newt_mod] = newton_mod(f, m_newt_mod, df, x0_newt_mod, tol, max_iter);
         results = [results; {'Newton Modificato', tol, root_newt_mod, iter_newt_mod, n_eval_newt_mod}];
