2466be992bc482b4662a76c6a2149aff6bcf3474
AleFeri <50620354+AleFeri@users.noreply.github.com>
2025-02-15 14:57:16 +0100

    finalizzato es 27

--- es27.m
+++ es27.m
@@ -1,23 +1,16 @@
-% Fissiamo il seme per la riproducibilità
 rng(0);
 
-% Generazione dei dati
 xi = linspace(0, 2*pi, 101);
 yi = sin(xi) + rand(size(xi)) *.05;
 
-% Calcolo dei coefficienti del polinomio di grado 3
-% I coefficienti sono ordinati a partire da quello relativo al termine di grado 3
-coeffs = polyfit(xi, yi, 3);
-% Il polinomio ottenuto è:
-%   p(x) = coeffs(1)*x^3 + coeffs(2)*x^2 + coeffs(3)*x + coeffs(4)
+coeff = polyfit(xi, yi, 3);
+
 disp('Coefficienti del polinomio di grado 3:');
-disp(coeffs);
+disp(coeff);
 
-% Calcolo dei valori del polinomio per un grafico più "liscio"
 xi_fit = linspace(0, 2*pi, 1000);
-yi_fit = polyval(coeffs, xi_fit);
+yi_fit = polyval(coeff, xi_fit);
 
-% Plot dei dati originali e del polinomio di approssimazione
 figure;
 plot(xi, yi, 'bo', 'MarkerSize', 6, 'DisplayName', 'Dati');
 hold on;
